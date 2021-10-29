a9120986403fd6588d6e0e399c63871bf7936f80
PrKsushka <ksushkaprot@gmail.com>
2021-10-29 16:41:00 +0300

    add changes

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use("/api/form",formRouter);
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'))
     })
 }
 
